36dff502af372d5dc411510960551820a298f061
JoeNatan30 <alulab14@V20723.lab.inf.pucp.edu.pe>
2015-11-03 17:10:21 -0500

    agregado campo en  accion especial para el tiempo de cada comando

--- src/Modelo/Mapa.java
+++ src/Modelo/Mapa.java
@@ -21,20 +21,20 @@ public class Mapa{
 	public Mapa(int nivel){		//Inicializa el mapa con caracteres en blanco
 		setListaAcciones(new ArrayList<AccionesEspeciales>());
 		if(nivel == 0){ //Tutorial
-			agregarCombinacion(0,  5,5,     9,5,"WSIKDDLL");
-			agregarCombinacion(1,  3,12,    9,11,"WDEWW");
-			agregarCombinacion(1,  4,12,    9,11,"WDEWW");
-			agregarCombinacion(1,  5,12    ,9,11,"WDEWW");
-			agregarCombinacion(1,  6,12    ,9,11,"WDEWW");
+			agregarCombinacion(0,  5,5,     9,5,"WSIKDDLL",5);
+			agregarCombinacion(1,  3,12,    9,11,"WDEWW",4);
+			agregarCombinacion(1,  4,12,    9,11,"WDEWW",4);
+			agregarCombinacion(1,  5,12    ,9,11,"WDEWW",4);
+			agregarCombinacion(1,  6,12    ,9,11,"WDEWW",4);
 		}
 		if(nivel == 1){ //Nivel 1 //Cuy_1  //Cuy_2
-			agregarCombinacion(0,   8,5,    10,5,"IWOELD");
-			agregarCombinacion(0,   8,10,   10,10,"WUOQEI");
+			agregarCombinacion(0,   8,5,    10,5,"IWOELD",5);
+			agregarCombinacion(0,   8,10,   10,10,"WUOQEI",5);
 		}
 		if(nivel == 2){ //Nivel 2
-			agregarCombinacion(0,   5,9,    7,9, "WQED");
-			agregarCombinacion(1,  -1,-1,   9,3,"IUOL");
-			agregarCombinacion(1,   3,3,    -1,-1,"LDOEQUOE");
+			agregarCombinacion(0,   5,9,    7,9, "WQED",3);
+			agregarCombinacion(1,  -1,-1,   9,3,"IUOL",3);
+			agregarCombinacion(1,   3,3,    -1,-1,"LDOEQUOE",5);
 		}
 		for(int fil=0;fil<12;fil++){
 			for(int col=0;col<16;col++){
@@ -44,8 +44,8 @@ public class Mapa{
 		
 	}
 	
-	public void agregarCombinacion(int tipo, int posXC1, int posYC1, int posXC2, int posYC2, String combinacion){
-		AccionesEspeciales accionNueva = new AccionesEspeciales(tipo, posXC1, posYC1, posXC2, posYC2, combinacion);
+	public void agregarCombinacion(int tipo, int posXC1, int posYC1, int posXC2, int posYC2, String combinacion,int tiempo){
+		AccionesEspeciales accionNueva = new AccionesEspeciales(tipo, posXC1, posYC1, posXC2, posYC2, combinacion,tiempo);
 		getListaAcciones().add(accionNueva);
 	}
 	
