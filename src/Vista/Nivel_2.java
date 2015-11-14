0bb138dd1eee8b2741582f9c8a166ab3941c6ce9
Zikokun <fmore91@gmail.com>
2015-11-14 15:29:19 -0500

    nivel 2,los inicios xD

--- src/Vista/Nivel_2.java
+++ src/Vista/Nivel_2.java
@@ -9,6 +9,6 @@ import javax.imageio.ImageIO;
 public class Nivel_2 {
 	public BufferedImage  imgFondo;		
 	public void cargarImagen() throws IOException{
-		imgFondo = ImageIO.read(new File("mapa_nivel_1.jpg")); // CAmbiar	
+		imgFondo = ImageIO.read(new File("mapa_nivel_2.jpg")); // CAmbiar	
 	}	
 }
